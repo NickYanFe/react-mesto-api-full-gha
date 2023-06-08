64d875dbbef6361e1b276ceb6500d4885a784e4a
Nikolay <nickyanfe@yandex.ru>
2023-06-08 12:43:48 +0300

    Закрашивание кнопки лайк

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -5,7 +5,7 @@ import CurrentUserContext from "../contexts/CurrentUserContext";
 function Card({ card, onCardClick, onCardDelete, onCardLike }) {
   const currentUser = useContext(CurrentUserContext);
   const isOwn = card.owner === currentUser._id; //определяем, являемся ли мы владельцем текущей карточки
-  const isLiked = card.likes.some((i) => i._id === currentUser._id); // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
+  const isLiked = card.likes.some((id) => id === currentUser._id); // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
   const cardLikeButtonClassName = `element__like-button ${
     isLiked && "element__like-button_active"
   }`; // Создаём переменную, которую после зададим в `className` для кнопки лайка
